9d2b0d624415fef90563695c0aaf4e33b5086a23
jeremy koundour <j.koundour@gmail.com>
2023-02-21 18:54:27 +0100

    test sur Recharts

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,8 +1,22 @@
-import './App.css';
+import './style/App.css';
+import { useEffect, useState } from 'react';
+import Chart from './components/Chart';
 
 function App() {
+  const [data, setData] = useState()
+  useEffect(()=> {
+    const fetchData = async () => {
+      const response = await fetch("https://api.coincap.io/v2/assets/?limit=10");
+      const data =await response.json()
+      console.log(data);
+      setData(data.data)
+    }
+    fetchData()
+  }, [])
   return (
     <div className="App">
+    hello!!
+    <Chart data={data}></Chart>
     </div>
   );
 }
